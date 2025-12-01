685716861fa5991e13ef7f0b60d2d5764d9e6412
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2025-12-01 11:09:32 +0400

    added js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let price= document.getElementById('price')
+let totalprice=document.getElementById('totalprice')
+function updatePrice() {
+    totalprice.innerHTML = `
+        <p class="text-[45px] font-semibold mb-3">${price.value}₼</p>
+    `;
+}
+updatePrice()
+  price.addEventListener("input", updatePrice);

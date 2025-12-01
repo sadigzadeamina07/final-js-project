d3183e9d8b1508cbab03f7a48cfc8145843e9105
sadigzadeamina7 <sadigzadeamina7@gmail.com>
2025-12-01 15:22:31 +0400

    added some cart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 let price= document.getElementById('price')
 let totalprice=document.getElementById('totalprice')
+let percent=document.getElementById('percent')
+let t=document.getElementById('totalprice')
 function updatePrice() {
     totalprice.innerHTML = `
         <p class="text-[45px] font-semibold mb-3">${price.value}₼</p>

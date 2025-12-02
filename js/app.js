eece78ef39a313ea48c0af7336ba1c12704593dd
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2025-12-03 02:21:35 +0400

    changed cards structure

--- js/app.js
+++ js/app.js
@@ -3,18 +3,13 @@ let percent=document.getElementById('percent')
 let time=document.getElementById('time')
 let totalprice=document.getElementById('totalprice')
   const links = document.querySelectorAll(".tab-link");
-    const cards = document.querySelectorAll(".card");
-    const rangeler = document.querySelectorAll(".rangeler");
-
+  const tabs = document.querySelectorAll(".tab-body");
     links.forEach(link => {
       link.addEventListener("click", (e) => {
         e.preventDefault();
 
            const target = link.getAttribute("href").replace("#", "");
-        // bütün kartları gizlət
-        cards.forEach(card => card.classList.add("hidden"));
-
-        // seçilmiş kartı göstər
+         tabs.forEach(tab => tab.classList.add("hidden"));
         document.getElementById(target).classList.remove("hidden");
       });
     });
@@ -22,9 +17,6 @@ function updatePrice() {
      const P = Number(price.value);           
     const n = Number(time.value);            
     const r = Number(percent.value)/12/100;  
-  // let monthlyprice=price.value/time.value
-  // let ededinfaizi=percent.value* monthlyprice /100;
-  // let total=monthlyprice+ededinfaizi;
     const total = P * (r * (1 + r)**n) / ((1 + r)**n - 1)
        totalprice.innerHTML = `
         <p class="text-[45px] font-semibold mb-1">${total.toFixed(2)}₼</p>
